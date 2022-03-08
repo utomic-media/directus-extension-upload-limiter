41839c8e78d143fa81c0958596fb8075fd3f1a7a
Dominic Marcelino <domi.m.98@googlemail.com>
2022-03-08 13:11:14 +0100

    Update migration: Align column naming with directus

--- src/_migrations/20220301-extension-upload-limiter.js
+++ src/_migrations/20220301-extension-upload-limiter.js
@@ -2,13 +2,13 @@ module.exports = {
 
 	async up(knex) {
 		await knex.schema.alterTable('directus_users', (table) => {
-			table.json('directus-extension-upload-limiter');
+			table.json('directus_extension_upload_limiter');
 		});
 	},
 
 	async down(knex) {
 		await knex.schema.alterTable('directus_users', (table) => {
-			table.dropColumn('directus-extension-upload-limiter');
+			table.dropColumn('directus_extension_upload_limiter');
 		});
 	},
 };
