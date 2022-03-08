072e2c0df3f6f4506ae420192d6bb21eac98fdf2
Dominic Marcelino <domi.m.98@googlemail.com>
2022-03-08 13:15:17 +0100

    Add basic action to calculate user-based file uploads

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,67 @@
 import { defineHook } from '@directus/extensions-sdk';
 
-export default defineHook(({ filter, action }) => {
+
+interface uploadLimiterOptions {
+	numberOfFiles: number,
+	aggregatedFilessize: bigint,
+}
 
 
-	// get upload-limit from env variables, set to fals if none is set
+export default defineHook(({ action }, { services, exceptions, env }) => {
 
-	// before upload: check if fits into the limit
+	// TODO: before upload: check if fits into the limit
 		// yes: upload file
 		// no: send error: "upload-limit: this file would exceed your upload limit"
 	
-	// after: update user column uploaded filedata
+	// TODO: move to db-config once a key-value store is available
+	// const globalUploadLimit = env.DIRECTUS_EXTENSION_UPLOAD_LIMITER_GLOBAL_LIMIT; // upload-limit in byte
+	
 
-	// filter('items.create', () => {
-	// 	console.log('Creating Item!');
-	// });
+	// after upload: update user column uploaded filedata
+	action('files.upload', async ({ payload }, { schema, accountability }) => {
+		const { UsersService } = services;
+		const { ServiceUnavailableException } = exceptions;
+		
+		const uploadUser = accountability?.user;
 
-	// action('items.create', () => {
-	// 	console.log('Item created!');
-	// });
+		// TODO: fetch unknonw user
+		if (uploadUser) { 
+			const usersService = new UsersService({
+				schema: schema,
+				accountability: {
+					admin: true, // TODO: find a better way to restrict this
+					...accountability
+				},
+			});
+			try {
+				const userItem = await usersService.readOne(uploadUser);
+				const currentUserUploadData: uploadLimiterOptions | null = JSON.parse(userItem.directus_extension_upload_limiter);
+
+				const numberOfFiles = (currentUserUploadData?.numberOfFiles)
+					? (currentUserUploadData.numberOfFiles + 1)
+					: 1;
+				
+				const aggregatedFilessize = (currentUserUploadData?.aggregatedFilessize)
+					? (currentUserUploadData.aggregatedFilessize + payload.filesize)
+					: payload.filesize;
+				
+				await usersService.updateOne(uploadUser, {
+					'directus_extension_upload_limiter': {
+						...currentUserUploadData,
+						numberOfFiles,
+						aggregatedFilessize,
+					}
+				});
+			} catch (error) {
+				throw new ServiceUnavailableException(error);
+			}
+		}
+	});
+
+
+	// TODO: after update: get filesize before, filesize after, calc diff and apply to value
+
+	// TODO: after delete: get old filesize and substract
+	
+
 });
